df6fc837470f5eab58967e4c5da50a573994cfd1
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 01:21:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,17 @@ def oxford_comma(array)
     "#{array.shift} and #{array.shift}"
   elsif array.size == 3
     "#{array.shift}, #{array.shift}, and #{array.shift}"
+  elsif array.size > 3
+    counter = 1
+
+    string = "#{array.shift}"
+
+    for counter in 1..array.size-1 do
+      string.concat(", ")
+      string.concat(" #{array.shift}")
+    end
+
+    string.concat("and #{array.shift}")
+
   end
 end
