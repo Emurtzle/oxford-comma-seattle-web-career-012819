6cb9a538b69ec39e1737677b5673cc88a88e2454
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 01:16:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     "#{array.shift} and #{array.shift}"
   elsif array.size == 3
-    "#{array.shift}, #{array.shift} and #{array.shift}"
+    "#{array.shift}, #{array.shift}, and #{array.shift}"
   end
 end
